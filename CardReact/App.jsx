dcc0d66141677abd97812031912e992c27a294f5
Saairaam21 <91055655+Saairaam21@users.noreply.github.com>
2025-03-03 12:16:30 +0530

    App.jsx

---
+++ CardReact/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./components/Card.jsx"; // Correct path
+import './App.css'; // Add if you have app-wide styles
+
+function App() {
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
